6373c75533f4ba39f2c65217d41df69bb525d5e3
plusmouse <plusmouse@protonmail.com>
2025-10-18 18:02:27 +0100

    Show ?? in level text widget if the mob is a boss

--- Display/LevelText.lua
+++ Display/LevelText.lua
@@ -7,7 +7,7 @@ function addonTable.Display.LevelTextMixin:SetUnit(unit)
   self.unit = unit
   if self.unit then
     self:RegisterUnitEvent("UNIT_LEVEL", self.unit)
-    self.text:SetText(UnitLevel(self.unit))
+    self:UpdateLevel()
   else
     self:Strip()
   end
@@ -17,6 +17,15 @@ function addonTable.Display.LevelTextMixin:Strip()
   self:UnregisterAllEvents()
 end
 
+function addonTable.Display.LevelTextMixin:UpdateLevel()
+  local level = UnitLevel(self.unit)
+  if level == -1 then
+    self.text:SetText("??")
+  else
+    self.text:SetText(level)
+  end
+end
+
 function addonTable.Display.LevelTextMixin:OnEvent(eventName, ...)
-  self.text:SetText(UnitLevel(self.unit))
+  self:UpdateLevel()
 end
