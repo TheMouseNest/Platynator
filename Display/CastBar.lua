5fc9e4e750656721321221ae7ce4c7aa7a406d2c
plusmouse <plusmouse@protonmail.com>
2025-10-23 12:52:28 +0100

    Smoother castbar for non-Midnight clients (ie those without secrets)

--- Display/CastBar.lua
+++ Display/CastBar.lua
@@ -65,10 +65,19 @@ function addonTable.Display.CastBarMixin:ApplyCasting()
 
   if type(startTime) ~= "nil" and type(endTime) ~= "nil" then
     self:Show()
-    self.statusBar:SetMinMaxValues(startTime, endTime)
-    self:SetScript("OnUpdate", function()
+    if issecretvalue and issecretvalue(startTime) then
+      self.statusBar:SetMinMaxValues(startTime, endTime)
+      self:SetScript("OnUpdate", function()
+        self.statusBar:SetValue(GetTime() * 1000)
+      end)
       self.statusBar:SetValue(GetTime() * 1000)
-    end)
+    else
+      self.statusBar:SetMinMaxValues(0, (endTime - startTime) / 1000)
+      self:SetScript("OnUpdate", function()
+        self.statusBar:SetValue(GetTime() - startTime / 1000)
+      end)
+      self.statusBar:SetValue(GetTime())
+    end
     self:ApplyColor(notInterruptible)
     C_Timer.After(0, function()
       if self.unit then
