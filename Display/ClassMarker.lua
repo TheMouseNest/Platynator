7c0d6b246f0944382e19e1418792fb118fe8be0c
plusmouse <plusmouse@protonmail.com>
2026-02-07 19:45:47 +0000

    Classic: Fix class icons

--- Display/ClassMarker.lua
+++ Display/ClassMarker.lua
@@ -3,6 +3,10 @@ local addonTable = select(2, ...)
 
 addonTable.Display.ClassMarkerMixin = {}
 
+local borderPool = CreateTexturePool(UIParent, "BACKGROUND", 0, nil, function(_, tex)
+  tex:SetColorTexture(0, 0, 0)
+end)
+
 local classMap = {
   ["DEATHKNIGHT"] = "DeathKnight",
   ["DEMONHUNTER"] = "DemonHunter",
@@ -19,17 +23,35 @@ local classMap = {
   ["WARLOCK"] = "Warlock",
 }
 
+function addonTable.Display.ClassMarkerMixin:PostInit()
+  self.border = borderPool:Acquire()
+  self.border:SetParent(self)
+  self.border:ClearAllPoints()
+
+  self.PostApplyAnchor = function()
+    PixelUtil.SetPoint(self.border, "TOPLEFT", self, "TOPLEFT", -1, 1)
+    PixelUtil.SetPoint(self.border, "BOTTOMRIGHT", self, "BOTTOMRIGHT", 1, -1)
+  end
+end
+
 function addonTable.Display.ClassMarkerMixin:SetUnit(unit)
   self.unit = unit
   if self.unit and (UnitIsPlayer(self.unit) or UnitTreatAsPlayerForDisplay and UnitTreatAsPlayerForDisplay(self.unit)) then
     self.marker:Show()
+    self.border:Show()
     local _, class = UnitClass(self.unit)
-    local atlas = "UI-HUD-UnitFrame-Player-Portrait-ClassIcon-" .. (classMap[class] or "")
+    local atlas = "classicon-" .. class:lower()
     self.marker:SetAtlas(atlas)
   else
     self.marker:Hide()
+    self.border:Hide()
   end
 end
 
 function addonTable.Display.ClassMarkerMixin:Strip()
+  if self.border then
+    self.border:Hide()
+    borderPool:Release(self.border)
+    self.border = nil
+  end
 end
