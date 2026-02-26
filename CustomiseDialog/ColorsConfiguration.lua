377eb8c52b8363d614df4b4f83e35db2ef11bcce
TheIceBadger <TheIceBadger@users.noreply.github.com>
2026-02-26 15:46:35 +0100

    Adds Has Debuff color option
    Adds a new color option to set color if there is a debuff on the unit, can be filtered like the debuff auras with "important" and "from you"

--- CustomiseDialog/ColorsConfiguration.lua
+++ CustomiseDialog/ColorsConfiguration.lua
@@ -666,6 +666,50 @@ addonTable.CustomiseDialog.ColorsConfig = {
       },
     }
   },
+  ["hasDebuff"] = {
+    label = addonTable.Locales.HAS_DEBUFF,
+    default = {
+      kind = "hasDebuff",
+      colors = {
+        hasDebuff = GetColor("FF6600"),
+      },
+      fromYou = false,
+      important = false,
+    },
+    entries = {
+      {
+        label = addonTable.Locales.HAS_DEBUFF,
+        kind = "colorPicker",
+        setter = function(details, value)
+          details.colors.hasDebuff = value
+        end,
+        getter = function(details)
+          return details.colors.hasDebuff
+        end,
+      },
+      { kind = "spacer" },
+      {
+        label = addonTable.Locales.FROM_YOU,
+        kind = "checkbox",
+        setter = function(details, value)
+          details.fromYou = value
+        end,
+        getter = function(details)
+          return details.fromYou
+        end,
+      },
+      {
+        label = addonTable.Locales.IMPORTANT,
+        kind = "checkbox",
+        setter = function(details, value)
+          details.important = value
+        end,
+        getter = function(details)
+          return details.important
+        end,
+      },
+    },
+  },
 }
 
 addonTable.CustomiseDialog.ColorsConfigOrder = {
@@ -681,6 +725,7 @@ addonTable.CustomiseDialog.ColorsConfigOrder = {
   "classColors",
   "difficulty",
   "execute",
+  "hasDebuff",
   "reaction",
   "interruptReady",
   "castTargetsYou",
