f99631d4836fc9737223b7af170cc2edff0ef559
plusmouse <plusmouse@protonmail.com>
2026-05-24 18:52:38 +0100

    Take talents into account when detecting pull range

--- Core/Constants.lua
+++ Core/Constants.lua
@@ -163,3 +163,11 @@ addonTable.Constants.DefaultRange = {
   [73] = 30, -- Protection
   [1446] = 30,
 }
+addonTable.Constants.RangeModifier = {
+  [276079] = 40, -- Death Knight, Death's Reach
+  [429211] = 40, -- Demon Hunter, Champion of the Glaive
+  [197524] = 45, -- Druid, Astral Influence
+  [459559] = 46, -- Priest, Phantom Reach
+  [428377] = 46, -- Rogue - Outlaw, Precision Shot
+  [1271948] = 35, -- Warrior, Javelineer
+}
