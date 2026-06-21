0f91eb90a81c914d46fd8ad786fa4e37cd7ccd27
plusmouse <plusmouse@protonmail.com>
2026-06-21 20:31:32 +0100

    Attempt to fix errors when nameplates created in Blizzard code

--- Display/Initialize.lua
+++ Display/Initialize.lua
@@ -95,10 +95,8 @@ function addonTable.Display.ManagerMixin:OnLoad()
             end
           end)
         end
-      elseif nameplate.UnitFrame.AurasFrame then -- Mists fix
-        nameplate.UnitFrame.AurasFrame:SetParent(addonTable.hiddenFrame)
       end
-      nameplate.UnitFrame:SetParent(addonTable.hiddenFrame)
+      nameplate.UnitFrame:Hide()
       nameplate.UnitFrame:UnregisterAllEvents()
       if nameplate.UnitFrame.castBar then
         nameplate.UnitFrame.castBar:UnregisterAllEvents()
